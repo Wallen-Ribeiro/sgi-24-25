23a2e18d7cd9ab19d1f0f7e2635a20f080f3643b
Pedro Oliveira <up202108669@up.pt>
2024-11-14 10:07:52 +0000

    Structure of graph visit done

--- tp2/parser/SceneGraph.js
+++ tp2/parser/SceneGraph.js
@@ -7,22 +7,22 @@ class SceneGraph {
         this.ambientLight = null;
     }
 
-        parse(data) {
-            const yasf = data['yasf'];
-            this.parseGlobals(yasf['globals'], yasf['fog']); // globals, fog and skybox
-            this.parseCameras(yasf['cameras']);
-            this.parseTextures(yasf['textures']);
-            this.parseMaterials(yasf['materials']);
-            //this.parseSkyBox(yasf['skybox'])
-            this.parseGraph(yasf['graph']);
-        }
+    parse(data) {
+        const yasf = data['yasf'];
+        this.parseGlobals(yasf['globals'], yasf['fog']); // globals, fog and skybox
+        this.parseCameras(yasf['cameras']);
+        this.parseTextures(yasf['textures']);
+        this.parseMaterials(yasf['materials']);
+        //this.parseSkyBox(yasf['skybox'])
+        this.parseGraph(yasf['graph']);
+    }
 
     parseGlobals(globals, fog) {
         const background = globals['background'];
         this.backgroundColor = new THREE.Color(background['r'], background['g'], background['b']);
 
         const ambient = globals['ambient'];
-        this.ambientLight = new THREE.AmbientLight(ambient['r'], ambient['g'], ambient['b'], ambient['intensity']);
+        this.ambientLight = new THREE.AmbientLight(new THREE.Color(ambient['r'], ambient['g'], ambient['b']), ambient['intensity']);
 
         this.fog = new THREE.Fog(fog['color']['r'], fog['color']['g'], fog['color']['b'], fog['near'], fog['far']);
     }
@@ -56,7 +56,7 @@ class SceneGraph {
             const texture = textures[textureId];
 
             this.textures[textureId] = new THREE.TextureLoader().load(texture['filepath']);
-            for(let i = 0; i <= 7 && texture[mipmapKeys[i]]; i++) {
+            for (let i = 0; i <= 7 && texture[mipmapKeys[i]]; i++) {
                 this.textures[textureId].mipmaps.push(texture[mipmapKeys[i]]);
             }
         });
@@ -76,7 +76,7 @@ class SceneGraph {
                 transparent: material['transparent'],
                 opacity: material['opacity'],
                 wireframe: material['wireframe'] ?? false,
-                shading: material['shading'] ?? 'smooth',
+                shading: material['shading'] ? 'flat' : 'smooth',
                 side: material['twosided'] ? THREE.DoubleSide : THREE.FrontSide,
                 map: material['textureref'] ?? 'null',
                 bumpMap: material['bumpscale'] ?? 'null',
@@ -88,7 +88,7 @@ class SceneGraph {
         });
     }
 
-    parseSkyBox(skybox){
+    parseSkyBox(skybox) {
         this.geometry = new THREE.BoxGeometry(skybox['size'], skybox['size'], skybox['size']);
 
         const ft = new THREE.TextureLoader().load(skybox['front']);
@@ -96,40 +96,99 @@ class SceneGraph {
         const up = new THREE.TextureLoader().load(skybox['up']);
         const dn = new THREE.TextureLoader().load(skybox['down']);
         const rt = new THREE.TextureLoader().load(skybox['right']);
-        const lf = new THREE.TextureLoader().load(skybox['left']);     
-        
-        
+        const lf = new THREE.TextureLoader().load(skybox['left']);
+
+
         const materials = [
-            new THREE.MeshBasicMaterial({map: rt, side: THREE.BackSide}),
-            new THREE.MeshBasicMaterial({map: lf, side: THREE.BackSide}),
-            new THREE.MeshBasicMaterial({map: up, side: THREE.BackSide}),
-            new THREE.MeshBasicMaterial({map: dn, side: THREE.BackSide}),
-            new THREE.MeshBasicMaterial({map: ft, side: THREE.BackSide}),
-            new THREE.MeshBasicMaterial({map: bk, side: THREE.BackSide})
+            new THREE.MeshBasicMaterial({ map: rt, side: THREE.BackSide }),
+            new THREE.MeshBasicMaterial({ map: lf, side: THREE.BackSide }),
+            new THREE.MeshBasicMaterial({ map: up, side: THREE.BackSide }),
+            new THREE.MeshBasicMaterial({ map: dn, side: THREE.BackSide }),
+            new THREE.MeshBasicMaterial({ map: ft, side: THREE.BackSide }),
+            new THREE.MeshBasicMaterial({ map: bk, side: THREE.BackSide })
         ];
 
         materials.forEach(material => {
-            material.emissive = new THREE.Color(skybox['emissive']['r'], skybox['emissive']['g'], skybox['emissive']['b']); 
+            material.emissive = new THREE.Color(skybox['emissive']['r'], skybox['emissive']['g'], skybox['emissive']['b']);
             material.emissiveIntensity = skybox['emissive']['intensity'];
         });
 
         this.skybox = new THREE.Mesh(this.geometry, materials);
         this.skybox.position.set(new THREE.Vector3(skybox['center']['x'], skybox['center']['y'], skybox['center']['z']));
-    
+
     }
 
     parseGraph(graph) {
-        // bfs
-        // visit node
-        // mark node has visited
-        // add children to queue
-        // node = queue.pop
+        const rootId = graph['rootid']
 
-        this.root = graph['rootId']
-        
-        //visitNode(root)
+        this.nodes = {};
+        this.modified = true;
+        while (this.modified) {
+            this.modified = false;
+            Object.keys(graph).forEach((nodeId) => {
+                this.visitNode(graph[nodeId], nodeId);
+            });
+        }
+
+        return this.nodes[rootId];
+
     }
 
+    visitNode(node, nodeId) {
+        if (this.nodes[nodeId]) {
+            if (node['children']) {
+                let group = this.nodes[nodeId];
+                for (const child of node['children']) {
+                    const childNode = this.visitNode(child, child['id']);
+                    if (childNode) {
+                        group.add(childNode);
+                    }
+                }
+            }
+
+            return null;
+        } else {
+            // not visited yet
+
+            const type = node['type'];
+
+            switch (type) {
+                case 'node':
+                    this.modified = true;
+                    group = new THREE.Group();
+                    this.nodes[nodeId] = group;
+
+                    // transforms (opt)
+                    // materialref (opt)
+                    // castshadows (opt)
+                    // receiveshadows (opt)
+                    for (const child of node['children']) {
+                        const childNode = this.visitNode(child, child['id']);
+                        if (childNode) {
+                            group.add(childNode);
+                        }
+                    }
+
+                    return group;
+                case 'noderef':
+                    if (this.nodes[node['nodeid']]) {
+                        this.modified = true;
+                        this.nodes[nodeId] = ""; // as its a ref, it should not be referenced? marking as visited only
+                        return this.nodes[node['nodeid']].clone();
+                    }
+                    return null;
+                default:
+                    this.modified = true;
+                    return createPrimitive(node, nodeId);
+            }
+
+        }
+
+    }
+
+    createPrimitive(node, nodeId) {
+
+    }
 }
 
 export { SceneGraph };
